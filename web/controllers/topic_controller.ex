8edb09fecc0b7986db7454c1eacf465f4b189106
Patrick Kostas <pa2kostas@yahoo.de>
2018-08-27 13:08:43 +0200

    Added topic controller

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,13 @@
+# by convention: singular form (not topics)
+defmodule Discuss.TopicController do
+  # elixir-way of code-sharing: importing functions defined (also imported) in Discuss.Web.controller/0
+  # equivalent of OOP inheritance
+  use Discuss.Web, :controller
+
+  def new(conn, params) do
+    IO.puts "++++"
+    IO.inspect conn
+    %Plug.Conn{conn | state: :set}
+  end
+
+end
