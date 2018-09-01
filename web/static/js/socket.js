d400ca0f1fdafb224fe1b0d49b9a22fe32918d97
Patrick Kostas <pa2kostas@yahoo.de>
2018-09-01 16:49:59 +0200

    Live-load comments and implement "add comment"

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -6,51 +6,6 @@
 import {Socket} from "phoenix"
 
 let socket = new Socket("/socket", {params: {token: window.userToken}})
-
-// When you connect, you'll often need to authenticate the client.
-// For example, imagine you have an authentication plug, `MyAuth`,
-// which authenticates the session and assigns a `:current_user`.
-// If the current user exists you can assign the user's token in
-// the connection for use in the layout.
-//
-// In your "web/router.ex":
-//
-//     pipeline :browser do
-//       ...
-//       plug MyAuth
-//       plug :put_user_token
-//     end
-//
-//     defp put_user_token(conn, _) do
-//       if current_user = conn.assigns[:current_user] do
-//         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-//         assign(conn, :user_token, token)
-//       else
-//         conn
-//       end
-//     end
-//
-// Now you need to pass this token to JavaScript. You can do so
-// inside a script tag in "web/templates/layout/app.html.eex":
-//
-//     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
-//
-// You will need to verify the user token in the "connect/2" function
-// in "web/channels/user_socket.ex":
-//
-//     def connect(%{"token" => token}, socket) do
-//       # max_age: 1209600 is equivalent to two weeks in seconds
-//       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
-//         {:ok, user_id} ->
-//           {:ok, assign(socket, :user, user_id)}
-//         {:error, reason} ->
-//           :error
-//       end
-//     end
-//
-// Finally, pass the token on connect as below. Or remove it
-// from connect if you don't care about authentication.
-
 socket.connect()
 
 const createSocket = (topicId) => {
@@ -62,6 +17,18 @@ channel.join()
   })
   .receive("error", resp => { console.log("Unable to join", resp) })
 
+  channel.on(`comments:${topicId}:new`, renderComment);
+
+  document.querySelector("button").addEventListener("click", () => {
+    const commentContent = document.querySelector("textarea").value;
+    channel.push("comment:add", {content: commentContent});
+
+  });
+}
+
+function renderComment(event) {
+  document.querySelector(".collection").innerHTML += commentTemplate(event.comment);
+}
 
 function renderComments(comments) {
   const renderedComments = comments.map(comment => {
@@ -69,3 +36,11 @@ function renderComments(comments) {
   });
   document.querySelector(".collection").innerHTML = renderedComments.join("");
 }
+
+function commentTemplate(comment) {
+  return `
+  <li class="collection-item">
+      ${comment.content}
+    </li>`
+}
+window.createSocket = createSocket;
