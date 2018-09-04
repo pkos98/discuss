e805f5fa2c5655eaa4c1d892e4619841e150fd03
Patrick Kostas <pa2kostas@yahoo.de>
2018-09-04 10:47:55 +0200

    Implement oauth callback-phase and session-handling

---
+++ web/controllers/auth_controller.ex
@@ -0,0 +1,49 @@
+defmodule Discuss.AuthController do
+  use Discuss.Web, :controller
+  plug Ueberauth
+  alias Discuss.User
+
+  def index(conn, _params) do
+  end
+
+  @doc """
+  Handles the callback-phase of the OAuth-process.
+  During this phase the provider (github) calls this route 
+  and it handles the successfull or failed authentication.
+  1. It looks at the result
+  2. On success, it looks if the user already signed in before
+     if not, the user is inserted into the DB.
+  3. Put the user-id into the session-cookie to persist as "logged in" state
+  4. Redirect to index
+  """
+  def callback(conn, %{"code" => oauth_code}) do
+    auth = conn.assigns.ueberauth_auth
+    user_params = %{email: auth.info.email, token: auth.credentials.token, provider: "github"}
+    changeset = %User{} |> User.changeset(user_params)
+    sign_in(conn, changeset)
+  end
+
+  def sign_in(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} -> 
+        conn
+        |> put_flash(:info, "Welcome back, #{user.email}")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} -> 
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(Discuss.User, email: changeset.changes.email) do
+      nil -> Repo.insert(changeset)
+      user -> {:ok, user}
+    end
+  end
+
+
+
+end
